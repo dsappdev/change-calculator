d1d6a2730cb966f10478eda9e51a6416b17168f4
Doug <dsgalocal@gmail.com>
2020-02-13 13:15:00 -0500

    Updated precision value of MathContext

--- src/com/dspohn/ChangeCalcRun.java
+++ src/com/dspohn/ChangeCalcRun.java
@@ -8,12 +8,10 @@ public class ChangeCalcRun {
 	public static void main(String[] args) {
 				
 		ChangeCalc cc = new ChangeCalc();
-
-		// Return the map and display it ordered.
-		BigDecimal amount = new BigDecimal(299.88);
-		System.out.println(cc.change(amount).toString());
-		System.out.println();
+				
+		BigDecimal amount = new BigDecimal(483.69, Denomination.mc);
 		Map<BigDecimal, Integer> change = cc.change(amount);
+		
 		System.out.println("Hundreds: " + change.get(Denomination.HUNDRED_DOLLAR) +
 				"\nFifties: " + change.get(Denomination.FIFTY_DOLLAR) +
 				"\nTwenties: " + change.get(Denomination.TWENTY_DOLLAR) +
@@ -24,6 +22,8 @@ public class ChangeCalcRun {
 				"\nDimes: " + change.get(Denomination.DIME) +
 				"\nNickels: " + change.get(Denomination.NICKEL) +
 				"\nPennies: " + change.get(Denomination.PENNY));
+		
+		
 		
 		
 	}
--- src/com/dspohn/Denomination.java
+++ src/com/dspohn/Denomination.java
@@ -3,12 +3,9 @@ package com.dspohn;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 
 public class Denomination {
-	private static MathContext mc = new MathContext(2, RoundingMode.HALF_UP);	
-	NumberFormat format = new DecimalFormat("0.###");	
+	public static MathContext mc = new MathContext(5, RoundingMode.HALF_UP);		
 	public static final BigDecimal HUNDRED_DOLLAR = new BigDecimal(100.0, mc);
 	public static final BigDecimal FIFTY_DOLLAR = new BigDecimal(50.0, mc);
 	public static final BigDecimal TWENTY_DOLLAR = new BigDecimal(20.0, mc);
